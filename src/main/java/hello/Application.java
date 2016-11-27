b058b6338ef40cc40fed0458777f25ac9e41b739
Wang, Sunny <Sunny.Wang@capitalone.com>
2016-11-27 09:49:14 -0500

    Add User, User Repository and User Rest Controller(adapted from beginning spring)

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="hello,com.sunnycorp.model,com.sunnycorp.repository,com.sunnycorp.controller")
 public class Application {
 
     public static void main(String[] args) {
